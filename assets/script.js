7232820cc174717f7765c32fe8f50abdd612c977
Brennan <brennanheley@gmail.com>
2022-03-25 00:33:57 -0500

    reformatted entire project with extra js file

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,5 @@
-var start = document.getElementById("start");
-var a = document.getElementById("a");
-var b = document.getElementById("b");
-var questions = ["what do people mean when they say camelcase?", "what must separate all items in an object?"];
-   var answers = ["a big box of cigarettes", " a suitcase for a desert animal", "a way of font styling", "$$$", "^", "!"];
-  var corrects = ["capitalizing Every Word After The First In Js To Make Reading Long Vars Easier", "a comma"];
+function quizHandlerStart () {
+    console.log('quizhandlerstart');
+}
 
-
- start.addEventListener('click', startQuiz);
-
- function startQuiz () {
-     a.textContent = corrects[0];
-     b.textContent = answers[1];
-
-
- }
+document.querySelector('start-quiz').addEventListener('click', quizHandlerStart);
