b03cd4ae14a37d1a2ae4e583ad4ac75be4b6df2a
Brennan <brennanheley@gmail.com>
2022-03-28 12:27:33 -0500

    worked on functions

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # CodingQuiz
 a timed quiz about basic coding, stores high scores. wrong answers subtract time from timer.
+
+
+// HTML 
+// start screen with title rules, start button, timer
+// question screen container with question text, buttons, timer
+// end of game container with final score, form to enter initials, and a submit button.
+//feed back based on answer selected
+// use local storage for high scores
+//separate high score page
+
+//js
+// declare variable to reference dom elements in your html document
+//  start quiz button, start screen element, timer el, high score el, 
+//  question title el, answer choices el
+
+//create an array of objects 
+//  answer choice
+// question text
+// correct answer
+
+//creat startquiz function triggered by start button
+    //hide start screen
+    //unhide questions screen
+    // get questions from array 
+    //start timer
+    // get first question from arr 
+    //
+
+//need function to getquestion(); parse info from js file
+    //append child render title
+
+//need function to check if an answer in correct
+    //if iscorrect is true 
+    //{move on to next question}
+    //if false decrement timer
+    //keep track of points
+    //increment index in array of questions to move on
+
+//function to end the quiZ
+    //hide question el
+    //unhide end of quiz el
+    //unhide final score el
+    //show submit button
+
+
+//sve high scores to local storage.
+    //button to try again
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,71 @@
-function quizHandlerStart () {
-    console.log('quizhandlerstart');
-}
+const a = document.querySelector('#a');
+const b = document.querySelector('#b');
+const c = document.querySelector('#c');
+const q = document.querySelector('h1');
+const subQ = document.querySelector('.sub-heading-text');
+const answers = document.querySelectorAll('h2');
+var n = 0;
+const startButton = document.querySelector('#start-quiz');
+var timeLeft = 10;
+var correctList = [1, 2, 1, 1,2];
+startButton.addEventListener('click', quizHandlerStart);
 
-document.querySelector('start-quiz').addEventListener('click', quizHandlerStart);
+
+
+
+function quizHandlerStart () {
+    getQuestion() ;
+  answerCheck ();
+
+
+   
+    }; 
+
+    function getQuestion () {
+        q.innerText = questionSet[n].question;
+    
+        subQ.innerText = "click your answer, wrong answers will remove time from the timer.";
+         
+        a.textContent = questionSet[n].answers[0].text;
+        
+         b.innerText = questionSet[n].answers[1].text;
+        
+        c.innerText = questionSet[n].answers[2].text;
+        
+        startButton.setAttribute('class', 'hidden');
+
+        answers[correctList[n]].addEventListener ("click", answerCheck);
+
+
+    }
+
+    function answerCheck () {
+
+    }
+    // function secondQ () {
+       
+    //    n+=1;
+       
+    //    q.innerText = questionSet[n].question;
+    //     subQ.innerText = "click your answer, wrong answers will remove time from the timer.";
+         
+    //     a.textContent = questionSet[n].answers[0].text;
+       
+        
+    //      b.innerText = questionSet[n].answers[1].text;
+        
+    //     c.innerText = questionSet[n].answers[2].text;
+        
+    //     startButton.setAttribute('class', 'hidden');
+
+        
+    // }
+
+
+    // function setWins () {
+    //     localStorage.setItem( );
+    // }
+    
+    // function setLosses () {
+    //     localStorage.setItem()
+    // }
